5e9b0ee1aa9e2b804aa6a62a80726f7996d0e7ff
Stars_Kim <stars_kim@163.com>
2018-12-20 03:49:26 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Stars_Kim';
         $user->email = 'stars_kim@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
